33c533d8caf5605e8fceac9948611c00e7993db2
Jorge Prieto <jorgeprg@correo.ugr.es>
2021-11-21 02:00:32 +0100

    Implementacion con BBDD (MySQL)
    He importado desde el tutorial https://spring.io/guides/gs/accessing-data-mysql/ el proyecto
    
    Desde el proyecto initial, he copiado los ficheros java que tenía en com.dss.p2

---
+++ practica2/src/main/java/com/example/accessingdatamysql/Product.java
@@ -0,0 +1,60 @@
+package com.example.accessingdatamysql;
+
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private long id;
+	
+	private String nombre;
+	private String marca;
+	private String descripcion;
+	private double precio;
+
+	public Product(String nombre, String marca, String descripcion, double precio) {
+		this.nombre = nombre;
+		this.marca = marca;
+		this.descripcion = descripcion;
+		this.precio = precio;
+	}
+	
+	public Product(String nombre, double precio) {
+		this(nombre, "", "", precio);
+	}
+	
+	public Product() {}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+}
---
+++ practica2/src/main/java/com/example/accessingdatamysql/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/catalogue")
+public class ProductController {
+	@Autowired
+	private ProductRepository productRepository;
+
+	@PostMapping(path="/add")
+	public @ResponseBody String addProduct (@RequestParam String name
+			, @RequestParam String marca
+			, @RequestParam String descripcion
+			, @RequestParam double precio) {
+
+		productRepository.save(new Product(name, marca, descripcion, precio));
+		return "Saved";
+	}
+
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+}
